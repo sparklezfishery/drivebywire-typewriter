785cb370178c05fef6326beb82f1094ece05090e
BingOAO233 <sparklezfishery@gmail.com>
2026-06-02 01:13:12 +0800

    feat: i18n optimization

--- src/main/java/org/sparklezfish/drivebywire/typewriter/TypewriterChannels.java
+++ src/main/java/org/sparklezfish/drivebywire/typewriter/TypewriterChannels.java
@@ -13,51 +13,51 @@ public final class TypewriterChannels {
         // Letters A-Z (GLFW codes match ASCII uppercase: 65-90)
         for (int c = 65; c <= 90; c++) {
             char ch = (char) c;
-            CODE_TO_CHANNEL.put(c, "create_submarine.key." + Character.toLowerCase(ch));
+            CODE_TO_CHANNEL.put(c, "drivebywiretypewriter.key." + Character.toLowerCase(ch));
         }
         // Digits 0-9 (GLFW 48-57)
         for (int c = 48; c <= 57; c++) {
             char ch = (char) c;
-            CODE_TO_CHANNEL.put(c, "create_submarine.key." + ch);
+            CODE_TO_CHANNEL.put(c, "drivebywiretypewriter.key." + ch);
         }
 
         // Symbol keys present in the Typewriter's keyboard UI
-        CODE_TO_CHANNEL.put(32,  "create_submarine.key.space");
-        CODE_TO_CHANNEL.put(39,  "create_submarine.key.apostrophe");
-        CODE_TO_CHANNEL.put(44,  "create_submarine.key.comma");
-        CODE_TO_CHANNEL.put(45,  "create_submarine.key.minus");
-        CODE_TO_CHANNEL.put(46,  "create_submarine.key.period");
-        CODE_TO_CHANNEL.put(47,  "create_submarine.key.slash");
-        CODE_TO_CHANNEL.put(59,  "create_submarine.key.semicolon");
-        CODE_TO_CHANNEL.put(61,  "create_submarine.key.equals");
-        CODE_TO_CHANNEL.put(91,  "create_submarine.key.left_bracket");
-        CODE_TO_CHANNEL.put(92,  "create_submarine.key.backslash");
-        CODE_TO_CHANNEL.put(93,  "create_submarine.key.right_bracket");
+        CODE_TO_CHANNEL.put(32,  "drivebywiretypewriter.key.space");
+        CODE_TO_CHANNEL.put(39,  "drivebywiretypewriter.key.apostrophe");
+        CODE_TO_CHANNEL.put(44,  "drivebywiretypewriter.key.comma");
+        CODE_TO_CHANNEL.put(45,  "drivebywiretypewriter.key.minus");
+        CODE_TO_CHANNEL.put(46,  "drivebywiretypewriter.key.period");
+        CODE_TO_CHANNEL.put(47,  "drivebywiretypewriter.key.slash");
+        CODE_TO_CHANNEL.put(59,  "drivebywiretypewriter.key.semicolon");
+        CODE_TO_CHANNEL.put(61,  "drivebywiretypewriter.key.equals");
+        CODE_TO_CHANNEL.put(91,  "drivebywiretypewriter.key.left_bracket");
+        CODE_TO_CHANNEL.put(92,  "drivebywiretypewriter.key.backslash");
+        CODE_TO_CHANNEL.put(93,  "drivebywiretypewriter.key.right_bracket");
         // Control keys
-        CODE_TO_CHANNEL.put(257, "create_submarine.key.enter");
-        CODE_TO_CHANNEL.put(258, "create_submarine.key.tab");
-        CODE_TO_CHANNEL.put(259, "create_submarine.key.backspace");
-        CODE_TO_CHANNEL.put(261, "create_submarine.key.delete");
-        CODE_TO_CHANNEL.put(280, "create_submarine.key.caps_lock");
+        CODE_TO_CHANNEL.put(257, "drivebywiretypewriter.key.enter");
+        CODE_TO_CHANNEL.put(258, "drivebywiretypewriter.key.tab");
+        CODE_TO_CHANNEL.put(259, "drivebywiretypewriter.key.backspace");
+        CODE_TO_CHANNEL.put(261, "drivebywiretypewriter.key.delete");
+        CODE_TO_CHANNEL.put(280, "drivebywiretypewriter.key.caps_lock");
 
         // Navigation
-        CODE_TO_CHANNEL.put(262, "create_submarine.key.right");
-        CODE_TO_CHANNEL.put(263, "create_submarine.key.left");
-        CODE_TO_CHANNEL.put(264, "create_submarine.key.down");
-        CODE_TO_CHANNEL.put(265, "create_submarine.key.up");
-        CODE_TO_CHANNEL.put(266, "create_submarine.key.page_up");
-        CODE_TO_CHANNEL.put(267, "create_submarine.key.page_down");
-        CODE_TO_CHANNEL.put(269, "create_submarine.key.end");
+        CODE_TO_CHANNEL.put(262, "drivebywiretypewriter.key.right");
+        CODE_TO_CHANNEL.put(263, "drivebywiretypewriter.key.left");
+        CODE_TO_CHANNEL.put(264, "drivebywiretypewriter.key.down");
+        CODE_TO_CHANNEL.put(265, "drivebywiretypewriter.key.up");
+        CODE_TO_CHANNEL.put(266, "drivebywiretypewriter.key.page_up");
+        CODE_TO_CHANNEL.put(267, "drivebywiretypewriter.key.page_down");
+        CODE_TO_CHANNEL.put(269, "drivebywiretypewriter.key.end");
 
         // Modifiers
-        CODE_TO_CHANNEL.put(340, "create_submarine.key.left_shift");
-        CODE_TO_CHANNEL.put(341, "create_submarine.key.left_ctrl");
-        CODE_TO_CHANNEL.put(342, "create_submarine.key.left_alt");
-        CODE_TO_CHANNEL.put(343, "create_submarine.key.left_super");
-        CODE_TO_CHANNEL.put(344, "create_submarine.key.right_shift");
-        CODE_TO_CHANNEL.put(345, "create_submarine.key.right_ctrl");
-        CODE_TO_CHANNEL.put(346, "create_submarine.key.right_alt");
-        CODE_TO_CHANNEL.put(348, "create_submarine.key.menu");
+        CODE_TO_CHANNEL.put(340, "drivebywiretypewriter.key.left_shift");
+        CODE_TO_CHANNEL.put(341, "drivebywiretypewriter.key.left_ctrl");
+        CODE_TO_CHANNEL.put(342, "drivebywiretypewriter.key.left_alt");
+        CODE_TO_CHANNEL.put(343, "drivebywiretypewriter.key.left_super");
+        CODE_TO_CHANNEL.put(344, "drivebywiretypewriter.key.right_shift");
+        CODE_TO_CHANNEL.put(345, "drivebywiretypewriter.key.right_ctrl");
+        CODE_TO_CHANNEL.put(346, "drivebywiretypewriter.key.right_alt");
+        CODE_TO_CHANNEL.put(348, "drivebywiretypewriter.key.menu");
     }
 
     public static final List<String> CHANNELS = List.copyOf(CODE_TO_CHANNEL.values());
